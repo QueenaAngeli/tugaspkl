ee362e2347e488848312efab7118b0f5facb3c45
MiloBreaker <queenaangely@gmail.com>
2023-11-23 10:49:28 +0700

    update php

--- save.php
+++ save.php
@@ -1,11 +1,11 @@
 <?php
 include 'conn.php';
-$id = $_GET['id'];
+// $id = $_GET['id'];
 $name = $_POST['name'];
 $msg = $_POST['msg'];
 if($name != "" && $msg != ""){
-	$sql = $conn->query("INSERT INTO discussion (parent_comment, student, post)
-			VALUES ('$id', '$name', '$msg')");
+	$sql = $conn->query("INSERT INTO discussion (student, post)
+			VALUES ('$name', '$msg')");
 	echo json_encode(array("statusCode"=>200));
 }
 else{
